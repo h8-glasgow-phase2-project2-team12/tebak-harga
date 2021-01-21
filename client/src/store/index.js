5801f9b4d66e75c4a50aa71c89de38b43474d4d1
amandarzq <amanda.rzq@gmail.com>
2021-01-21 21:49:31 +0700

    client login

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,14 +1,27 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import router from '../router/index'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    player: {
+      username: '',
+      score: ''
+    }
   },
   mutations: {
+    addPlayer (state, payload) {
+      state.player.username = payload.username
+      state.player.score = payload.score
+    }
   },
   actions: {
+    enterGame (context, payload) {
+      router.push('/dashboard')
+      context.commit('addPlayer', payload)
+    }
   },
   modules: {
   }
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div>
+  <main>
+    <div class="container">
+      <h1 class="title"> Tebak Harga</h1>
+    </div>
+    <div class="sign-in-form">
+      <div class="form" id="sign-in">
+        <form>
+          <h2>Play Game</h2>
+          <div class="input form-group">
+            <label for="username">USERNAME</label>
+            <input  v-model="user.username" type="text" id="username">
+          </div>
+          <div class="input form-group">
+            <input @click.prevent="enterGame" id="sign-in-btn" class="sign-in-page btn" type="submit" value="Main">
+          </div>
+        </form>
+      </div>
+    </div>
+  </main>
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      user: {
+        username: '',
+        score: 0
+      }
+    }
+  },
+  methods: {
+    enterGame () {
+      this.$store.dispatch('enterGame')
+      this.$store.dispatch('addPlayer', this.user)
+    }
   }
 }
 </script>
