94ea65b061977409655abcfff1daa5f6f6c8eaf6
phitonthel <thephitonthel@gmail.com>
2021-01-21 20:39:08 +0700

    added Dashboard.vue

---
+++ client/src/views/Dashboard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Tebak Harga!</h1>
+    <div class="row itemsContainer">
+      <ItemCard
+        v-for="item in items"
+        :key="item.id"
+        :item="item"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import ItemCard from '../components/ItemCard'
+
+export default {
+  components: {
+    ItemCard
+  },
+  computed: {
+    items () {
+      return this.$store.state.items
+    }
+  }
+}
+</script>
+
+<style>
+.itemsContainer {
+  justify-content: center;
+}
+</style>
